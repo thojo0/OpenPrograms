5637c6fb2711e3b05308cc4ecef11126f5b78e43
Thojo0 <53666000+thojo0@users.noreply.github.com>
2021-03-28 23:00:17 +0200

    Update tank-controll.lua

--- tank-controll.lua
+++ tank-controll.lua
@@ -2,19 +2,19 @@ tank = require("component").tinker_tank
 args = {...}
 
 -- getFluids to array
-function getfluids()
+function getFluids()
   fluids = {}
   for _, fluid in pairs(tank.getFluids()) do
-    fluids[table.getn(fluids)] = fluid
+    fluids[table.getn(fluids) + 1] = fluid
   end
   return fluids
 end
 -- search for fluid with string
 function search(fluids, search)
   gefunden = {}
-  for i=0, table.getn(fluids) - 1 do
+  for i=1, table.getn(fluids) do
     if string.find(string.lower(fluids[i].label .. fluids[i].name), string.lower(search)) then
-      gefunden[table.getn(gefunden)] = i
+      gefunden[table.getn(gefunden) + 1] = i
     end
   end
   return gefunden
@@ -22,20 +22,20 @@ end
 
 -- Subcommands
 if string.lower(args[1]) == "get" then  -- GET befehl
-  fluids = getFluids()
-  for i=0, table.getn(fluids) - 1 do
-    print(i + 1 .. "\t" .. fluids[i].label)
+  fluids = {getFluids()}
+  for i=1, table.getn(fluids) do
+    print(i .. "\t" .. fluids[i].label)
   end
 end
 if string.lower(args[1]) == "set" then  -- SET befehl
-  fluids = getFluids()
+  fluids = {getFluids()}
   print("Welchen Fluid willst du?")
   search = io.read()
   
-  gefunden = search(fluids, search)
+  gefunden = {search(fluids, search)}
   if table.getn(gefunden) > 0 then
-    for i=0, table.getn(gefunden) - 1 do
-      print(i + 1 .. "\t" .. fluids[gefunden[i]].label)
+    for i=1, table.getn(gefunden) do
+      print(i .. "\t" .. fluids[gefunden[i]].label)
     end
     print("Bitte Index-Nummer eingeben:")
     idx = tonumber(io.read())
