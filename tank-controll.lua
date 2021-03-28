7e1e4f0be0eb92edfba77e47aec009d93808351c
Thojo0 <53666000+thojo0@users.noreply.github.com>
2021-03-28 20:32:13 +0200

    Update tank-controll.lua

--- tank-controll.lua
+++ tank-controll.lua
@@ -7,20 +7,25 @@ for _, fluid in pairs(fluids) do
   anzahl = anzahl + 1
 end
 
-print("Was willste?")
+print("Welchen Fluid willst du?")
 search = io.read()
 
+gefunden = false
 for _, fluid in pairs(fluids) do
   anzahl = anzahl - 1
   idx = idx + 1
   if anzahl > 0 then
-    if string.find(string.lower(fluid.label .. fluid.name), search) then
+    if string.find(string.lower(fluid.label .. fluid.name), string.lower(search)) then
       print(idx .. "\t" .. fluid.label)
+      gefunden = true
     end
   end
 end
 
-print("Index plz:")
-idx = io.read()
-
-tank.moveFluidToBottom(idx)
+if gefunden then
+  print("Index plz:")
+  idx = io.read()
+  tank.moveFluidToBottom(tonumber(idx))
+else
+  print("Fluid nicht gefunden!")
+end
