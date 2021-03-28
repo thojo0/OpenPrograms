1aad2749022c832b20b021e8f6c82e263d27cc72
Thojo0 <53666000+thojo0@users.noreply.github.com>
2021-03-28 20:17:55 +0200

    Update tank-controll.lua

--- tank-controll.lua
+++ tank-controll.lua
@@ -13,7 +13,7 @@ for _, fluid in pairs(fluids) do
   anzahl = anzahl - 1
   idx = idx + 1
   if anzahl > 0 then
-    if string.find(fluid.label .. fluid.name, search) then
+    if string.find(string.lower(fluid.label .. fluid.name), search) then
       print(idx .. "\t" .. fluid.label)
     end
   end
