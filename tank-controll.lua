8b35a1c34584ccc8fb387b6d80cbb216539ff83c
Thojo0 <53666000+thojo0@users.noreply.github.com>
2021-03-28 22:26:34 +0200

    Update tank-controll.lua

--- tank-controll.lua
+++ tank-controll.lua
@@ -1,31 +1,47 @@
 tank = require("component").tinker_tank
 
--- getFluids to array
-fluids = {}
-anzahl = 0
-for _, fluid in pairs(tank.getFluids()) do
-  fluids[anzahl] = fluid
-  anzahl = anzahl + 1
+if string.lower(args[0]) == "get" then  -- GET befehl
+  fluids = getFluids()
+  for i=0, table.getn(fluids) - 1 do
+    print(i + 1 .. "\t" .. fluids[i].label)
+  end
+  goto exit
 end
 
-print("Welchen Fluid willst du?")
-search = io.read()
-
-gefunden = false
-for i=0, anzahl do
-  if i < anzahl - 1 then
-    if string.find(string.lower(fluids[i].label .. fluids[i].name), string.lower(search)) then
-      print(i + 1 .. "\t" .. fluids[i].label)
-      gefunden = true
+if string.lower(args[0]) == "set" then  -- SET befehl
+  fluids = getFluids()
+  print("Welchen Fluid willst du?")
+  search = io.read()
+  
+  gefunden = search(fluids, search)
+  if table.getn(gefunden) > 0 then
+    for i=0, table.getn(gefunden) - 1 do
+      print(i + 1 .. "\t" .. fluids[gefunden[i]].label)
     end
+    print("Bitte Index-Nummer eingeben:")
+    idx = tonumber(io.read())
+    tank.moveFluidToBottom(idx)
+  else
+    print("Fluid nicht gefunden!")
   end
 end
 
-idx = 0
-if gefunden then
-  print("Bitte Index-Nummer eingeben:")
-  idx = io.read()
-  tank.moveFluidToBottom(tonumber(idx))
-else
-  print("Fluid nicht gefunden!")
+-- getFluids to array
+function getfluids()
+  fluids = {}
+  for _, fluid in pairs(tank.getFluids()) do
+    fluids[table.getn(fluids)] = fluid
+  end
+  return fluids
+end
+
+-- search for fluid with string
+function search(fluids, search)
+  gefunden = {}
+  for i=0, table.getn(fluids) - 1 do
+    if string.find(string.lower(fluids[i].label .. fluids[i].name), string.lower(search)) then
+      gefunden[table.getn(gefunden)] = i
+    end
+  end
+  return gefunden
 end
