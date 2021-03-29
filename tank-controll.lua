1f2064f1139e687d6ac6c84ffdadf3e33ed13c9e
Thojo0 <53666000+thojo0@users.noreply.github.com>
2021-03-30 00:41:41 +0200

    Update tank-controll.lua

--- tank-controll.lua
+++ tank-controll.lua
@@ -4,6 +4,12 @@ tank = components.tinker_tank
 gpu = components.gpu
 args = {...}
 
+-- runden
+function round(num, numDecimalPlaces)
+  local mult = 10^(numDecimalPlaces or 0)
+  return math.floor(num * mult + 0.5) / mult
+end
+
 -- getFluids to array
 function getFluids()
   fluids = {}
@@ -34,13 +40,14 @@ if string.lower(args[1]) == "get" then  -- GET befehl
     print(i .. "\t" .. fluids[i].label)
   end
   stand = (resY - 2) * tank.getFillLevel() / tank.getCapacity()
+  standR = round(stand)
   color = gpu.getBackground()
   gpu.setBackground(0xCCCCCC)
   gpu.fill(resX - grafiksize + 1, 1, grafiksize, resY, " ")
   gpu.setBackground(0x0000FF)
-  gpu.fill(resX - grafiksize + 2, stand + 1, grafiksize - 2, resY - stand - 2, " ")
+  gpu.fill(resX - grafiksize + 2, standR + 1, grafiksize - 2, resY - standR - 2, " ")
   gpu.setBackground(color)
-  gpu.fill(resX - grafiksize + 2, 2, grafiksize - 2, stand, " ")
+  gpu.fill(resX - grafiksize + 2, 2, grafiksize - 2, standR, " ")
   io.read()
 end
 if string.lower(args[1]) == "set" then  -- SET befehl
